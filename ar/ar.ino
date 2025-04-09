6ee6e80c9ffc581a1787a05ed94bf808c46944d9
stellaaa42 <sssosaa42@gmail.com>
2025-04-09 14:03:43 +0300

    lastbutton=high if last=high and button=low ++ last=button

--- ar/ar.ino
+++ ar/ar.ino
@@ -1,10 +1,43 @@
+int ledPin1 = 9;
+int ledPin2 = 10;
+int buttonPin = 2;
+
+int brightness = 0;
+int fadeAmount = 10;
+
+int buttonState = 0;
+int lastButtonState = HIGH;
+
 void setup() {
-    Serial.begin(9600);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // Käytetään sisäistä pullupia
+
+  brightness = 0;
+  buttonState = 0;
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  // Tarkistetaan onko nappia juuri painettu (reunatunnistus)
+  if (lastButtonState == HIGH && buttonState == LOW) {
+    brightness += fadeAmount;
+
+    if (brightness > 255) {
+      brightness = 0; // Palautetaan nollaan, kun yli maksimin
+    }
+
+    analogWrite(ledPin1, brightness);
+    analogWrite(ledPin2, brightness);
+
+    Serial.print("PWM-arvo (brightness): ");
+    Serial.println(brightness);
+
+    delay(200); // Debounce-viive
   }
-  
-  void loop() {
-    int sensorValue = analogRead(A0);        
-    int scaledValue = map(sensorValue, 0, 1023, 0, 10); 
-    Serial.println(scaledValue);             
-    delay(500);          
-  }gtia
+
+  lastButtonState = buttonState;
+}
