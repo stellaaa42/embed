d9f282a97950b0e7629d47daae596f8c57493ac4
stellaaa42 <sssosaa42@gmail.com>
2025-04-15 12:49:29 +0300

    port binary

--- ar/ar.ino
+++ ar/ar.ino
@@ -1,26 +1,19 @@
-// stella 15.4.2025
-const byte ledPin[] = {7, 8, 9, 10, 11, 12, 13}; // A–G (järjestyksessä)
-
-byte i = 0; // for-loopin muuttuja
-
-void setup() {
-  // Määritellään pinneille OUTPUT-tila
-  for (i = 0; i < 7; i++) {
-    pinMode(ledPin[i], OUTPUT);
-  }
-
-  // Näytetään numero 3: segmentit a, b, c, d ja g päälle (HIGH)
-  digitalWrite(ledPin[0], HIGH); // a
-  digitalWrite(ledPin[1], HIGH); // b
-  digitalWrite(ledPin[2], HIGH); // c
-  digitalWrite(ledPin[3], HIGH); // d
-  digitalWrite(ledPin[6], HIGH); // g
-
-  // Loput segmentit pois (LOW): e ja f
-  digitalWrite(ledPin[4], LOW);  // e
-  digitalWrite(ledPin[5], LOW);  // f
+void setup()
+{
+  DDRB = B00111111; // Asetetaan Uno pinnit 8–13 (PB0–PB5) output-tilaan
 }
 
-void loop() {
-  // Ei toistoa tarvitse
+void loop()
+{
+  // LEDit PB0 → PB5
+  for (int i = 0; i <= 5; i++) {
+    PORTB = (1 << i);  // Sytytetään yksi ledi kerrallaan
+    delay(200);
+  }
+
+  // LEDit PB4 → PB1 (vältetään PB5 kahdesti)
+  for (int i = 4; i >= 0; i--) {
+    PORTB = (1 << i);
+    delay(200);
+  }
 }
