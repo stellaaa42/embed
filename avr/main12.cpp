ed5f055a05bdb6edcb22c3bafcd87ed92950f3fe
stellaaa42 <sssosaa42@gmail.com>
2025-05-16 16:15:09 +0300

    lfsr 8-bit linear feedback shift register xor change bits the most 2^8=256 255 psudorandomness delay rnd cycles asychoronous with the loop

--- avr/main12.cpp
+++ avr/main12.cpp
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	unsigned char noppa;
+	unsigned char nop;
 	unsigned char run;
 	unsigned char rnd;
 	unsigned char i;
@@ -33,36 +33,40 @@ int main(void)
 	PORTB = 0x7F; // high
 	// _delay_ms(500);
 	
-	noppa = 0;
+	nop = 0;
 	run   = 0;
 	i     = sizeof(dly)/sizeof(dly[0]); // 12*2/2 int 2/4 bytes 
 	
-	rnd = 0; // increment every 75ms
+	rnd = 0; // pseudorandom 8-bit linear feedback shift register/XOR change bits
+	// 2^8	at most 255 non-zero
+	// increment every 75ms 
 
 
 	while (1)
 	{
 		// if (!S1)
 		// if (PIND & (1<<PD2))
-		if (!PIND.2) // btn high -> low asynchronous with the loop, appears random
+		if (!PIND.2) // once btn high -> low, asynchronous with the loop, appears random
 		{
 			run = MIN + rnd % 32; // 'roll' 100+0 100+1..131 7.5ms-8.9ms
-			PORTB = taulukko[noppa]; // if btn each cycle run-- -> 0 stop at current
-			// _delay_ms(200);
 		}
 
 		if (run)
 		{
-			run--; // hit 0 before 255
-			noppa++; // 1 2...7
-			if (noppa == 7)
+			run--; // 100,99,...0 hit 0 before 255, then MCU sleeps until next btn
+			nop++; // 1 2...7
+			if (nop == 7)
 			{
-				noppa = 0;
+				nop = 0;
 			}
 		}
 
+		PORTB = taulukko[nop]; // display current, when run-- -> 0 stop at that num
 		// _delay_ms(200);
-		_delay_loop_1(200); // run>12 200 iterations 125x3x200 -> 75 microsec/us 8-bit counter up to 256
+
+		// _delay_ms(200);
+		_delay_loop_1(200); // counter++, cpu sits in the loop++, delay++
+		// busy_wait for a bit, run>12 200 iterations 125x3x200 -> 75 microsec/us 8-bit counter up to 256
 		rnd++; // 1 cycle 0-255 256->0 every 256x75=19.2milliseconds/ms 
 		
 		if (run > 0 && run < i + 1) // 1-12 <=12
