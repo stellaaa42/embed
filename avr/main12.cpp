b44baabe4c87e6b3e959aa3cda3f462f0cedfac0
stellaaa42 <sssosaa42@gmail.com>
2025-05-20 08:57:28 +0300

    lightshow i iterate, if btn, fix to a num

--- avr/main12.cpp
+++ avr/main12.cpp
@@ -14,7 +14,6 @@ int main(void)
 	unsigned char run;
 	unsigned char rnd;
 	unsigned char i;
-	unsigned char num;
 	unsigned char taulukko[] = {
 		0x06, // 1
 		0x5B, // 2
@@ -29,26 +28,32 @@ int main(void)
 	DDRD = 0x00; // input
 	PORTD |= (1<<PD2); // pull-up
 	
-	DDRB  = 0x7F; // output 
+	DDRB  = 0x7F; // output
 	PORTB = 0x7F; // high
 	// _delay_ms(500);
 	
 	nop = 0;
 	run   = 0;
-	i     = sizeof(dly)/sizeof(dly[0]); // 12*2/2 int 2/4 bytes 
+	i     = sizeof(dly)/2; // 12*2/2 int 2/4 bytes
 	
 	rnd = 0; // pseudorandom 8-bit linear feedback shift register/XOR change bits
 	// 2^8	at most 255 non-zero
-	// increment every 75ms 
+	// increment every 75ms
 
 
 	while (1)
 	{
-		// if (!S1)
-		// if (PIND & (1<<PD2))
-		if (!PIND.2) // once btn high -> low, asynchronous with the loop, appears random
+		for (i = 0; i < 6; i++)
+		{
+			PORTB = taulukko[i];
+			_delay_ms(100);
+		}
+		if (PIND & (1<<PD2))
+		// once btn high -> low, asynchronous with the loop, appears random
 		{
 			run = MIN + rnd % 32; // 'roll' 100+0 100+1..131 7.5ms-8.9ms
+			PORTB = taulukko[nop]; // display current, when run-- -> 0 stop at that num
+			_delay_ms(500);
 		}
 
 		if (run)
@@ -61,13 +66,9 @@ int main(void)
 			}
 		}
 
-		PORTB = taulukko[nop]; // display current, when run-- -> 0 stop at that num
-		// _delay_ms(200);
-
-		// _delay_ms(200);
 		_delay_loop_1(200); // counter++, cpu sits in the loop++, delay++
 		// busy_wait for a bit, run>12 200 iterations 125x3x200 -> 75 microsec/us 8-bit counter up to 256
-		rnd++; // 1 cycle 0-255 256->0 every 256x75=19.2milliseconds/ms 
+		rnd++; // 1 cycle 0-255 256->0 every 256x75=19.2milliseconds/ms
 		
 		if (run > 0 && run < i + 1) // 1-12 <=12
 		{
