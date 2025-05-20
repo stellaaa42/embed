5288322b6ad597300baa024a56c4642631da0751
stellaaa42 <sssosaa42@gmail.com>
2025-05-20 18:19:54 +0300

    digitalWrite(led, val) analog-digital

--- avr/arduino_code/alarm.ino
+++ avr/arduino_code/alarm.ino
@@ -1,15 +1,23 @@
-// attiny85 pin6-> arduino 1
-const byte Btn = 3;
-const byte alarm = 1;
+// attiny85 pin6 - arduino 1 
+// pin5 - arduino 0
+// pin3 - arduino A2
+int btn = A2;
+const byte led = 1;
+
+int val = 0;
 
 
 void setup() {
-    pinMode(Btn);
+    pinMode(btn, INPUT);
+    pinMode(led, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop() {
-    if (digitalRead(Btn) == HIGH) {
-        digitalWrite(alarm) == HIGH;
-    }
+    // val = analogRead(btn);
+    val = digitalRead(btn);
+    Serial.prinln(val);
+    delay(200);
+
+    digitalWrite(led, val);
 }
