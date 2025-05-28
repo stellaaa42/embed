e78d46367e9cedaaa29c8f41c9ea640d8bf81144
stellaaa <sssosaa42@gmail.com>
2025-05-28 14:40:22 +0300

    Update alarm.ino

--- avr/arduino_code/alarm.ino
+++ avr/arduino_code/alarm.ino
@@ -1,7 +1,7 @@
 // attiny85 pin3 - arduino4&a2
 // pin5 - arduino0 
 // pin6 - arduino1
-// pin2-arduino3&a3, pin1-reset, pin7-arduino2pin2-arduino3, pin1-reset, pin7-arduino2&a1
+// pin2-arduino3&a3, pin1-reset, pin7-arduino2, pin2-arduino3, pin1-reset, pin7-arduino2&a1
 const int motion = 4;
 const int led = 1;
 const int buzzer = 0;
