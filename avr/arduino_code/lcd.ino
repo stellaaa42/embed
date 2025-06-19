e3c0aabe14de1da6210ccc4b793a2cd054f35269
stellaaa <sssosaa42@gmail.com>
2025-06-19 09:16:03 +0300

    Add files via upload

--- avr/arduino_code/lcd.ino
+++ avr/arduino_code/lcd.ino
@@ -1,6 +1,10 @@
 #include <LiquidCrystal.h>
 
-
+// pot:3,77kohm 
+// uno_uno: const int rs=12, en=11, d4=5, d5=4, d6=3, d7=2;
+// 861_uno: const int rs=12, en=13, d4=7, d5=8, d6=9, d7=18;
+// 861_sch: const int rs = 1, en = 2, d4 = 3, d5 = 4, d6 = 7, d7 = 8;
+// uno_sch: const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
 const int rs=12, en=11, d4=5, d5=4, d6=3, d7=2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 String str="hello, world!";
